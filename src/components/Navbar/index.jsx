c980e8f05f0f061000f44f03dc4b40b367550544
Satch5918 <satch_elfante@hotmail.com>
2023-11-22 02:37:36 -0300

    props

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 const Navbar = () => {
+    const [search, setSearch] = useState('')
+const handleInputChange = (event) => {
+    setSearch(event.target.value)
+}
+
 return (
 <div>
     <p>Mi boletera</p>
-    <input placeholder="Buscar"  type="text" />
+    <input 
+    placeholder="Buscar"   
+    onChange={handleInputChange}/>
+    value={search}
 </div>
 )
 }
